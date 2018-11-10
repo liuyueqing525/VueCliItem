81b096011d26f50c2f3350c0ac5dd2494b8cf22f
LiuYueQing <505876704@qq.com>
2018-11-10 13:49:57 +0800

    Vue脚手架工具创建项目，demo

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Vuexx/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Vuexx from './components/vuex.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Vuexx
   }
 }
 </script>
